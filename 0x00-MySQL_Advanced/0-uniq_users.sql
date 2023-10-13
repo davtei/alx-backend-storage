d4271ae3e724d20b4ec796cf623915596f7513d0
David Tei <daveteidt@gmail.com>
2023-10-13 23:52:14 +0000

    add uniq_users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,6 @@
 -- Creates a table, users, with id, email, and name columns
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
+    name VARCHAR(255)
 );
