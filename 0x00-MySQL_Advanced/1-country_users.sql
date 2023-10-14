9f593970f3abe553b047765760c2e8277878a721
David Tei <daveteidt@gmail.com>
2023-10-14 00:04:34 +0000

    add country_users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table, users, with id, email, name and country columns
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
