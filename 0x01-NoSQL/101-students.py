a87065332558bf6822e8b6eda2a4c6fca0c3b943
David Tei <daveteidt@gmail.com>
2023-10-18 02:41:32 +0000

    sort by avg

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,23 +5,23 @@
 
 def top_students(mongo_collection):
     """Returns all students sorted by average score"""
-    students = mongo_collection.aggregate([
-        {
-            "$project": {
-                "_id": 1,
-                "name": 1,
-                "averageScore": {
-                    "$avg": {
-                        "$avg": "$topics.score"
-                    }
-                }
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
             },
-            "topics": 1
-        },
-        {
-            "$sort": {
-                "averageScore": -1
-            }
-        }
-    ])
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
     return students
