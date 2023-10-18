2266b9fdfe8d834444c77135ed15ed68aab39e0d
David Tei <daveteidt@gmail.com>
2023-10-18 02:57:07 +0000

    print nginx logs with top 10 requesting ips

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Log stats """
+
+from pymongo import MongoClient
+
+
+def print_nginx_req_logs(nginx_collection):
+    """ Prints nginx request logs stats with top 10 IPs """
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        req_count = len(list(nginx_collection.find({"method": method})))
+        print("\tmethod {}: {}".format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({"method": "GET", "path": "/status"})))
+    print("{} status check".format(status_checks_count))
+
+
+def print_top_10_ips(server_collection):
+    """ Prints top 10 IPs with most requests """
+    print("IPs:")
+    req_logs = server_collection.aggregate([
+        {
+            "$group": {"_id": "$ip", "count": {"$sum": 1}}
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 10
+        },
+        ])
+
+
+def run():
+    """ Runs print_nginx_req_logs """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_req_logs(client.logs.nginx)
+    print_top_10_ips(client.logs.nginx)
+
+
+if __name__ == "__main__":
+    run()
