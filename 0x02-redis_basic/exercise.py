0f35f7198bde622c4e6ebe88166f8ebad38c0aa0
David Tei <daveteidt@gmail.com>
2023-10-18 22:21:41 +0000

    recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis """
 
+import re
 import redis
 import uuid
-from typing import Union, Callable, Optional
+from typing import Union, Callable
 
 
 class Cache:
@@ -21,3 +22,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """ Convert data back to desired format """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """ Convert data to string """
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """ Convert data to int """
+        data = self._redis.get(key)
+        try:
+            return int(data.decode('utf-8'))
+        except Exception:
+            return 0
